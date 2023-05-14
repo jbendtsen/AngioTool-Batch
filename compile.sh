34a7f9dbc4a3597856f9c058e6384dd9a81df0fe
Jack Bendtsen <jackdbendtsen@gmail.com>
2023-05-14 04:19:19 +0000

    Set Java version to 8

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ rm -rf build
 mkdir build
 cd source
 find . | grep "\.java" > sources.txt
-$JAVAC -d ../build -cp lib/AbsoluteLayout.jar:lib/ij.jar:lib/Jama-1.0.2.jar:lib/mail.jar:lib/markSlider.jar:lib/poi-3.7-20101029.jar:. @sources.txt
+$JAVAC -source 8 -target 8 -d ../build -cp lib/AbsoluteLayout.jar:lib/ij.jar:lib/Jama-1.0.2.jar:lib/mail.jar:lib/markSlider.jar:lib/poi-3.7-20101029.jar:. @sources.txt
 [[ $? -ne 0 ]] && exit
 rm ../AngioTool-Batch.jar
 find images icons META-INF doc | zip AngioTool-Batch.jar -@
