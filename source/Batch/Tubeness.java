cafa43da1dc9ec07a118b6588dfd6bb9e940aaa5
Jack Bendtsen <jackdbendtsen@gmail.com>
2024-02-07 10:42:11 +0000

    Fixed Tubeness, integrated ComputeEigenValuesAtPoint2D into Tubeness

--- source/Batch/Tubeness.java
+++ source/Batch/Tubeness.java
@@ -2,6 +2,8 @@ package Batch;
 
 public class Tubeness
 {
+    public static final int IN_PLACE_THRESHOLD = 5;
+
     public static void computeTubenessImage(
         ISliceRunner sliceRunner,
         byte[] output,
@@ -18,75 +20,49 @@ public class Tubeness
         for (int i = 0; i < area; i++)
             image[i] = (float)(input[i] & 0xff);
 
-        float[] eigenOutput = FloatBufferPool.acquireAsIs(area);
+        float[] maxEigenOutput = FloatBufferPool.acquireZeroed(area);
 
-        float minResult = Float.MAX_VALUE;
-        float maxResult = Float.MIN_VALUE;
+        float[] gaussianOutput = FloatBufferPool.acquireAsIs(area);
+        float[] scratchBuf = FloatBufferPool.acquireAsIs(area);
+
+        double maxResult = 0.0;
         for (int s = 0; s < nSigmas; s++) {
-            /*
-            ImageProcessor ipp = original.getProcessor().duplicate();
-            ComputeCurvatures c = new ComputeCurvatures();
-            c.setData(new ImagePlus("", new FloatProcessor(width, height)));
-            c.setSigma(this.sigma[s]);
-            ComputeCurvatures.FloatArray2D fa2dInput = c.ImageToFloatArray(ipp);
-            ComputeCurvatures.FloatArray2D fa2d = c.computeGaussianFastMirror(fa2dInput, (float)this.sigma[s], null, calibration);
-            */
-
-            // eigenOutput is used as scratch. Its contents are only useful after computeEigenvalues() is called
-            computeGaussianFastMirror(image, eigenOutput, width, height, sigma[s], pixelWidth, pixelHeight);
-
-            final int threshold = -1;
-            ComputeEigenValuesAtPoint2D.computeEigenvalues(
-                sliceRunner,
-                Analyzer.MAX_WORKERS,
-                eigenOutput,
+            computeGaussianFastMirror(
+                gaussianOutput,
                 image,
+                scratchBuf,
                 width,
                 height,
                 sigma[s],
-                threshold
+                pixelWidth,
+                pixelHeight
             );
-
-            for(int i = 0; i < area; ++i) {
-                image[i] = Math.max(image[i], eigenOutput[i]);
-                /*
-                if (eigenOutput[i] > image[i]) {
-                    image[i] = eigenOutput[i];
-                }
-
-                if (slice[i] < minResult) {
-                    minResult = slice[i];
-                }
-
-                if (slice[i] > maxResult) {
-                    maxResult = slice[i];
-                }
-                */
-            }
+            double highestValue = computeEigenvalues(
+                sliceRunner,
+                Analyzer.MAX_WORKERS,
+                maxEigenOutput,
+                gaussianOutput,
+                width,
+                height,
+                sigma[s]
+            );
+            maxResult = Math.max(maxResult, highestValue);
         }
 
+        float factor = maxResult > 0.0 ? (float)(255.0 / maxResult) : 1.0f;
+
         for (int i = 0; i < area; i++)
-            output[i] = (byte)image[i];
+            output[i] = (byte)(Math.max(maxEigenOutput[i] * factor, 255.0f));
 
-        FloatBufferPool.release(eigenOutput);
+        FloatBufferPool.release(maxEigenOutput);
+        FloatBufferPool.release(gaussianOutput);
+        FloatBufferPool.release(scratchBuf);
         FloatBufferPool.release(image);
-
-        /*
-        long end = System.currentTimeMillis();
-        FloatProcessor fp = new FloatProcessor(width, height);
-        fp.setPixels(slice);
-        stack.addSlice(null, fp);
-
-        ImagePlus result = new ImagePlus("processed " + original.getTitle(), stack);
-        result.setCalibration(calibration);
-        result.getProcessor().setMinAndMax((double)minResult, (double)maxResult);
-        result.updateAndDraw();
-        return result;
-        */
     }
 
     // scratch MUST NOT ALIAS image
     private static void computeGaussianFastMirror(
+        float[] output,
         float[] image,
         float[] scratch,
         int width,
@@ -133,14 +109,14 @@ public class Tubeness
                 float avg = 0.0f;
                 for (int f = -ksHalfY; f <= ksHalfY; f++) {
                     // yy := mirror(y+f)
-                    //   int yf = abs(y+f) % (2*width)
-                    int yf = (y+f - (((y+f) >> 31) & ((y+f) << 1))) % ((width-1) << 1);
-                    int cond = (width - 1 - yf) >> 31;
-                    //   int yy = yf >= width ? 2*width - yf : yf;
-                    int yy = ((cond ^ yf) + cond) + (cond & (width << 1));
+                    //   int yf = abs(y+f) % (2*height)
+                    int yf = (y+f - (((y+f) >> 31) & ((y+f) << 1))) % ((height-1) << 1);
+                    int cond = (height - 1 - yf) >> 31;
+                    //   int yy = yf >= height ? 2*height - yf : yf;
+                    int yy = ((cond ^ yf) + cond) + (cond & (height << 1));
                     avg += kernelY[f + ksHalfY] * scratch[x + width * yy];
                 }
-                image[x + width * y] = avg;
+                output[x + width * y] = avg;
             }
         }
 
@@ -183,5 +159,119 @@ public class Tubeness
 
         for (int i = 0; i < size; ++i)
             kernel[i] /= sum;
+    }
+
+    static double computeEigenvalues(
+        ISliceRunner runner,
+        int maxWorkers,
+        float[] output,
+        float[] input,
+        int width,
+        int height,
+        double sigma
+    ) {
+        double highestValue = 0.0;
+        try {
+            Params params = new Params(input, width, height, sigma, 3, output, maxWorkers);
+            runner.runSlices(
+                params,
+                maxWorkers,
+                width,
+                IN_PLACE_THRESHOLD - 1
+            );
+            highestValue = params.finalMaximum;
+        }
+        catch (Throwable ignored) {}
+        return highestValue;
+    }
+
+    static float findSecondHessianEigenvalueAtPoint2D(float[] data, int width, int x, int y, double sigma)
+    {
+        double s2 = sigma * sigma;
+        int pos = x + width*y;
+        float dblCenter = 2.0F * data[pos];
+        float corners = (
+            ((data[pos+1+width] - data[pos-1+width]) / 2.0f) -
+            ((data[pos+1-width] - data[pos-1-width]) / 2.0f)
+        ) / 2.0f;
+
+        // mA = matrix[0][0]
+        // mB = matrix[0][1]
+        // mC = matrix[1][0]
+        // mD = matrix[1][1]
+        double mA = (data[pos+1] - dblCenter + data[pos-1]) * s2;
+        double mB = corners * s2;
+        double mC = mB;
+        double mD = (data[pos+width] - dblCenter + data[pos-width]) * s2;
+
+        double a = 1.0;
+        double b = -(mA + mD);
+        double c = mA * mD - mB * mB;
+        double discriminant = b * b - 4.0 * a * c;
+
+        if (discriminant < 0.0)
+            return 0.0f;
+
+        double dR = Math.sqrt(discriminant);
+        float e0 = (float)((-b + dR) / (2.0 * a));
+        float e1 = (float)((-b - dR) / (2.0 * a));
+
+        // return the second value, accounting for inverse
+        return Math.abs(e0) <= Math.abs(e1) ? e1 : e0;
+    }
+
+    static class Params implements ISliceCompute
+    {
+        public final float[] data;
+        public final int width;
+        public final int height;
+        public final double sigma;
+        public final float threshold;
+        public final float[] output;
+        public final double[] maximums;
+        public double finalMaximum;
+
+        public Params(float[] data, int width, int height, double sigma, float threshold, float[] output, int maxWorkers)
+        {
+            this.data = data;
+            this.width = width;
+            this.height = height;
+            this.sigma = sigma;
+            this.threshold = threshold;
+            this.output = output;
+            this.maximums = new double[maxWorkers];
+            this.finalMaximum = 0.0;
+        }
+
+        @Override
+        public Object computeSlice(int sliceIdx, int start, int length)
+        {
+            int end = start + length;
+            if (start <= 0)
+                start = 1;
+            if (end >= width - 1)
+                end = width - 2;
+
+            for (int y = 1; y < height - 1; ++y) {
+                for (int x = start; x < end; ++x) {
+                    int index = x + width*y;
+                    if (data[index] > threshold) {
+                        float ev = findSecondHessianEigenvalueAtPoint2D(data, width, x, y, sigma);
+                        if (ev < 0.0f) {
+                            output[index] = Math.max(output[index], -ev);
+                            maximums[sliceIdx] = Math.max(maximums[sliceIdx], -ev);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public void finishSlice(ISliceCompute.Result res)
+        {
+            finalMaximum = Math.max(finalMaximum, maximums[res.idx]);
+        }
     }
 }
